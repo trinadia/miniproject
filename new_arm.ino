7af53c32d9fa691ede854c7c20dad2836bdea6ff
Trinadia K <91521969+trinadia@users.noreply.github.com>
2023-09-29 15:11:11 +0700

    Update new_arm.ino

--- new_arm.ino
+++ new_arm.ino
@@ -1,43 +1,38 @@
-// consider usingn potentiometer
 #include <Servo.h>
+#define joyX1 A0
+#define joyY1 A1
+#define joyX2 A2
+#define joyY2 A3
+//const int grip_pin = 2;
+const int servopin1 = 3; //servo1
+const int servopin2 = 5; //servo2
+const int servopin3 = 6; //servo3
+const int servopin4 = 9; //base
+const int servopin5 = 10; //base
+const int trigPin = 7;
+const int echoPin = 8;
 
-Servo servo1, servo2, servo3, servoBase, gripper; 
-int servoPin[] = {3,5,6,9,10};
-// int buttonPin[] = {2,4,7,8,12,13};
-const int potpin1 = A0;
-const int potpin2 = A1;
-const int potpin3 = A2;
-const int potpin4 = A3;
-//const int pin_left = 2;
-//const int pin_right = 4;
-const int grip_pin = 12;
-const int switchPin = 11; // NOT PUSHBUTTON
-const int trigPin = 7;  
-const int echoPin = 8; 
+int angle1, angle2, angle3, angleBase, moveTo, safezone = 0;
+int detected_distance = 5;
 
-int angle1, angle2, angle3, angle4;
-int baseAngle = 0;
-bool isManual = true;
-bool lastButtonState = LOW;
+int xValue1 = 0; // To store value of the X axis
+int yValue1 = 0;
+int xValue2 = 0;
+int yValue2 = 0;
 
+Servo servo1, servo2, servo3, servoBase, gripper;
 void setup() {
-  //for (int i = 0; i < 6;i++){ // forward, backward, left, right, up, down
-    //pinMode(buttonPin[i], INPUT);
-    //}
-  pinMode(grip_pin, INPUT);
-  pinMode(switchPin, INPUT);
-  servo1.attach(servoPin[0]);
-  servo2.attach(servoPin[1]);
-  servo3.attach(servoPin[2]);
-  servoBase.attach(servoPin[3]);
-  gripper.attach(servoPin[4]);
-
+  Serial.begin(9600);
+  servo1.attach(servopin1);
+  servo2.attach(servopin2);
+  servo3.attach(servopin3);
+  servoBase.attach(servopin4);
+  gripper.attach(servopin5);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  
-  //set sudut awal
-  servo1.write(0); 
-  servo2.write(0); 
+
+  servo1.write(0);
+  servo2.write(0);
   servo3.write(0);
   servoBase.write(0);
   gripper.write(0);
@@ -67,55 +62,65 @@ void loop() {
 }
 // --------- MANUAL FUNCTIONS --------
 void manualMode(){
-  
-  // MANUAL TASKS
-  // control individual servos
-  int potentio1 = analogRead(potpin1);
-  int potentio2 = analogRead(potpin2);
-  int potentio3 = analogRead(potpin3);
-  int potentio4 = analogRead(potpin4);
-  // int left = digitalRead(pin_left);
-  // int right = digitalRead(pin_right);
-  int gripping = digitalRead(grip_pin);
-  
-  angle1 = map(potentio1, 0, 1023, 0, 180);
-  angle2 = map(potentio2, 0, 1023, 0, 180);
-  angle3 = map(potentio3, 0, 1023, 0, 180);
-  angle4 = map(potentio4, 0, 1023, 0, 180);
-  
+  xValue1 = analogRead(joyX1);
+  yValue1 = analogRead(joyY1);
+  xValue2 = analogRead(joyX2);
+  yValue2 = analogRead(joyY2);
+ 
+  //print the values with to plot or view
+  Serial.print(xValue1);
+  Serial.print("\t");
+  Serial.println(yValue1);
 
-  if (gripping == 1){
+  //int degreeX = map(xValue, 0, 1023, 0, 180);
+  //int degreeY = map(yValue, 0, 1023, 0, 180);
+  int degreeX = 5;
+  int degreeY = 5;
+  
+  if (xValue1 > 512){ // right
+    angleBase = angleBase + degreeX;
+  }
+  if (xValue1 < 512){ // left
+    angleBase = angleBase - degreeX;
+  }
+  if (yValue1 > 512){ // forward
+    angle1 = angle1 - degreeY; // lower
+    angle2 = angle2 + degreeY; 
+    angle3 = angle3 + degreeY;
+  }
+  if (yValue1 < 512){ // backward
+    angle1 = angle1 + degreeY;
+    angle2 = angle2 - degreeY;
+    angle3 = angle3 - degreeY;
+  }
+  // joystick 2
+  if (yValue2 > 512){ // up
+    angle1 = angle1 + degreeY;
+    angle2 = angle2 - degreeY;
+    angle3 = angle3 + degreeY;
+  } 
+  if (yValue2 < 512){ // down
+    angle1 = angle1 - degreeY;
+    angle2 = angle2 + degreeY;
+    angle3 = angle3 + degreeY;
+  }
+  if (xValue2 < 512 || xValue2 > 512){ // ready to pickup/release
     gripper.write(90);
   }
-  else if (gripping == 0){
+  else if (xValue2 == 512){ // grip
     gripper.write(10);
   }
-  
+
   servo1.write(angle1);
   servo2.write(angle2);
   servo3.write(angle3);
-  servoBase.write(angle4);
-  delay(50); 
+  servoBase.write(angleBase);
+  delay(5); 
 }
 
 void autoMode() {
-  int readAngle1 = servo1.read();
-  int readAngle2 = servo2.read();
-  int readAngle3 = servo3.read();
   int readBase = servoBase.read();
-  int readGrip = gripper.read();
-  
-  // set base servo to face toward danger zone 1 first
-  // scanning mechanism:
-  // if the servo already faces certain zone but no object detected, move to another zone
-  // read servo + conditioning distance from sensor
-  
-  servoBase.write(0);
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(0);
-  gripper.write(0);
-  
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -123,65 +128,106 @@ void autoMode() {
   digitalWrite(trigPin, LOW);
 
   long duration = pulseIn(echoPin, HIGH);
-  long distance = (duration*.0342)/2;
+  long distance = (duration * 0.0342) / 2;
 
-  // while loop to add the angle gradually
-  int max_angle = 120;
-  int i = 0;
-  while (i < max_angle){
-    servo1.write(i);
-    servo2.write(i);
-    servo3.write(i);
-    i++;
-    if (distance < 5){
-      i = 120;
+  // Check for target detection within 5 cm
+  if (distance < detected_distance) {
+    stopServos();
+    rescue();
+    delay(1000); // Delay for stability (adjust as needed).
+
+  } else {
+    // Move servos to 120 degrees until a target is detected
+    for (int i = 0; i <= 120; i++) {
+
+      // Check for target detection
+      if (distance < detected_distance) {
+        // If the target is detected stop scanning and perform rescue.
+        stopServos();
+        rescue();
+        delay(1000); // Delay for stability (adjust as needed).
+        break; // Exit the scanning loop.
+      }
+      servo1.write(i);
+      servo2.write(i);
+      servo3.write(i);
+
+      delay(20); // Adjust the delay as needed to control servo speed.
+    }
+    if (servo1.read() == 120 && servoBase.read() == 0 && distance > detected_distance) {
+      servoBase.write(90);
+    }
+    else if (servo1.read() == 120 && servoBase.read() == 90 && distance > detected_distance) {
+      servoBase.write(45);
+    }
+    else if (servo1.read() == 120 && servoBase.read() == 45 && distance > detected_distance) {
+      servoBase.write(0);
     }
   }
-  
-
-  // ZONE 1
-  if (distance < 5 && readBase == 0){
-    // give command to gripper
-    rescue(90);
-    searching(90);
-  } else if (distance > 10 && readBase == 0){ // no target found
-    searching(45);
-  }
-  // ZONE 2
-  if (distance < 5 && readBase == 90){
-    // give command to gripper
-    rescue(90);
-  }
-  
   delay(100);
 }
-void rescue(int degree){
+void stopServos() {
+  servo1.write(servo1.read()); // Stop servo1.
+  servo2.write(servo2.read()); // Stop servo2.
+  servo3.write(servo3.read()); // Stop servo3.
+}
+void rescue() {
   int gripAngle = 45;
   int liftAngle = 180;
   int liftPlace = 90;
+  int moveFrom = servoBase.read();
+  if (moveFrom == 0) {
+    safezone = 25; // safe zone 1
+    moveTo = 90;
+  }
+  if (moveFrom == 90) {
+    safezone = 65;
+    moveTo = 45;
+  }
+  if (moveFrom == 45) {
+    safezone = moveFrom;
+    liftPlace = 45;
+    moveTo = 0;
+  }
+
   // GRIP
-  gripper.write(90); // hold
-  servo1.write(gripAngle);
-  servo2.write(gripAngle);
-  servo3.write(gripAngle);
-  delay(50);
+  gripper.write(90); // ready to grip
+  servo1.write(servo1.read());
+  servo2.write(servo2.read());
+  servo3.write(servo3.read());
+  delay(1000);
+  gripper.write(10); // taken
+  delay(1000);
+
   // LIFT
   servo1.write(liftAngle);
   servo2.write(liftAngle);
   servo3.write(liftAngle);
-  // MOVE
-  servoBase.write(degree);
+  delay(1000);
+
+  // PLACE ON SAFE ZONE
+  servoBase.write(safezone);
+  delay(1000);
+
   // PLACE
   servo1.write(liftPlace);
   servo2.write(liftPlace);
   servo3.write(liftPlace);
-  gripper.write(90); //open
+  gripper.write(90); // Open
+  delay(1000);
+
+  //MOVE
+  servoBase.write(moveTo);
   delay(1000);
 }
-void searching(int setpoint){
-  servoBase.write(setpoint);
-  servo1.write(10);
-  servo2.write(10);
-  servo3.write(10);
-  gripper.write(90);
+void searching() {
+  if (servoBase.read() == 0) {
+    servoBase.write(90);
+  }
+  if (servoBase.read() == 90) {
+    servoBase.write(45);
+  }
+  if (servoBase.read() == 45) {
+    servoBase.write(0);
+  }
 }
